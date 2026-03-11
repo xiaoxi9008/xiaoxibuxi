a341b9962302896711194da56b9085977aa6b638
xuaoxi9008 <3574769415@qq.com>
2026-03-11 13:28:48 +0800

    Add files via upload

---
+++ 服务器id加载.lua
@@ -0,0 +1,32 @@
+ local function safeNotify(title, text, duration)
+     pcall(function()
+         game:GetService("StarterGui"):SetCore("SendNotification", {
+             Title = title,
+             Text = text,
+             Duration = duration or 1,
+             Button1 = "OK"
+         })
+     end)
+ end
+ safeNotify("正在执行XIAOXI", "正在启动脚本...", 1.5)
+ wait(1.5)
+ safeNotify("小西脚本", "正在检测服务器...", 1)
+if game.PlaceId == 18687417158 then --- 服务器id
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Xiaoxi/refs/heads/main/NOL-%E4%BB%98%E8%B4%B9%E7%89%88%E6%9C%80%E6%96%B0%E6%BA%90%E7%A0%81.lua"))()---该服务器脚本
+
+ elseif game.PlaceId == 7239319209 then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/tnine%E4%BF%84%E4%BA%A5%E4%BF%84%E5%B7%9E%E6%BA%90%E7%A0%81%20(1).lua"))() -- Ohio 服务器脚本
+     
+elseif game.PlaceId == 286090429 then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E5%85%B5%E5%B7%A5%E5%8E%82.lua"))()
+
+elseif game.PlaceId == 79393329652220 then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E9%98%B2%E5%BE%A1.lua"))() 
+else
+
+elseif game.PlaceId == 94101948530988 then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E8%B5%9B%E9%A9%AC%E5%A8%98%E3%80%82.lua"))() 
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E5%8A%A0%E8%BD%BD%E5%99%A8%E3%80%82.lua"))()---加载通用raw
+
+end---结束
