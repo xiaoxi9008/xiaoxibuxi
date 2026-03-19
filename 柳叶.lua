22853493900c4f0944af0b7aa088c2c97184ab69
xuaoxi9008 <3574769415@qq.com>
2026-03-19 21:41:43 +0800

    Add files via upload

---
+++ 柳叶.lua
@@ -0,0 +1,63 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+local Window = WindUI:CreateWindow({
+    Title = "柳叶",
+    Icon = "rbxassetid://123691280552142",
+    Author = "by小西制作",
+    AuthorImage = 90840643379863,
+    Folder = "CloudHub",
+    Size = UDim2.fromOffset(560, 360),
+    Background = "video:https://raw.githubusercontent.com/xiaoxi9008/xiaoxibuxi/refs/heads/main/video_260319_213124.mp4",
+    User = {
+            Enabled = true,
+            Callback = function()
+                WindUI:Notify({
+                    Title = "点击了自己",
+                    Content = "没什么", 
+                    Duration = 1,
+                    Icon = "4483362748"
+                })
+            end,
+            Anonymous = false
+        },
+})
+
+Window:EditOpenButton(
+    {
+        Title = "爸爸",
+        Icon = "rbxassetid://123691280552142",
+        CornerRadius = UDim.new(0, 13),
+        StrokeThickness = 4,
+        Color = ColorSequence.new({ColorSequenceKeypoint.new(0, Color3.fromRGB(186, 19, 19)),ColorSequenceKeypoint.new(1, Color3.fromRGB(8, 60, 129))}),
+        Draggable = true
+    }
+)
+
+function Tab(a)
+    return Window:Tab({Title = a, Icon = "eye"})
+end
+
+function Button(a, b, c)
+    return a:Button({Title = b, Callback = c})
+end
+
+function Toggle(a, b, c, d)
+    return a:Toggle({Title = b, Value = c, Callback = d})
+end
+
+function Slider(a, b, c, d, e, f)
+    return a:Slider({Title = b, Step = 1, Value = {Min = c, Max = d, Default = e}, Callback = f})
+end
+
+function Dropdown(a, b, c, d, e)
+    return a:Dropdown({Title = b, Values = c, Value = d, Callback = e})
+end
+
+function Input(a, b, c, d, e, f)
+    return a:Input({
+        Title = b,
+        Desc = c or "",
+        Value = d or "",
+        Placeholder = e or "",
+        Callback = f
+    })
+end
